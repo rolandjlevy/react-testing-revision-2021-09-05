4faf83bf83514d753745d389a8223386e7b78b47
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-09-06 22:20:31 +0000

    Writing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('to do app', () => {
@@ -12,5 +13,18 @@ describe('to do app', () => {
     expect(input).toHaveValue('');
     const button = screen.getByRole('button', {name: 'Add'});
     expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+
+    const todoDescription = 'Hello, World!';
+    userEvent.type(input, todoDescription);
+    expect(button).not.toBeDisabled();
+
+    userEvent.click(button);
+    const todo = screen.getByText(todoDescription);
+    expect(todo).toBeInTheDocument();
+    
+    // const removeButton = screen.getByLabelText('remove-btn');
+    // const removeButton = screen.getByText( { 'aria-remove': 2 }));
+    // expect(removeButton).toBeInTheDocument();
   });
 });
